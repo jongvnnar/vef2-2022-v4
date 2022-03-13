1c1d405bacba2cf9961c238d0119b50c7c4bc4c9
jongvnnar <jgh060@gmail.com>
2022-03-13 19:34:28 +0000

    add loading

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,3 +1,45 @@
+import { useEffect, useState } from "react";
+import { Loading } from "../components/Loading/Loading";
+
+const apiUrl = process.env.REACT_APP_API_URL;
 export function Index() {
-  return <h1>Index!</h1>;
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [events, setEvents] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      setLoading(true);
+      setError(null);
+      let json;
+      const url = new URL("/events", apiUrl);
+      try {
+        const result = await fetch(url);
+        console.log(result);
+        if (!result.ok) {
+          throw new Error("Error fetching events");
+        }
+        json = await result.json();
+      } catch (e) {
+        console.warn("Unable to fetch events", e);
+        setError("Ekki tókst að sækja viðburði");
+        return;
+      } finally {
+        setLoading(false);
+      }
+
+      console.log(json);
+      setEvents(json.items);
+    }
+    fetchData();
+  }, []);
+  if (loading) {
+    return <Loading />;
+  }
+  return (
+    <>
+      <p>{loading.toString()}</p>
+      <p>{JSON.stringify(error)}</p>
+      <p>{JSON.stringify(events)}</p>
+    </>
+  );
 }
